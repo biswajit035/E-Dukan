6134a9b64606235f86259954671426dd5e18b62b
Biswajit Debnath <biswajit.basanti@gmail.com>
2023-01-19 23:21:00 +0530

    clear code

---
+++ src/Layouts/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Style/css/main.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nobis natus laudantium non, autem eveniet ipsum doloribus fugit sequi? Minus quam quo incidunt maxime alias dolores odit vero laudantium eligendi accusamus, veniam perferendis est debitis a hic dicta voluptates? Dolores ea reiciendis alias provident veritatis porro quidem quam repellendus ut doloribus. Impedit eaque quidem pariatur dolores, architecto minima. Blanditiis fuga natus minima nesciunt ea, expedita, ducimus debitis officiis, repudiandae itaque necessitatibus a amet sequi nihil nulla eum asperiores excepturi unde facere tempora voluptates in eveniet sint libero. Cum fuga aut ipsa quisquam reiciendis eaque fugit. Saepe alias eveniet illum quos?
+    </div>
+  )
+}
+
+export default Navbar
